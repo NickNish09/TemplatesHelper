3bc340c29528c97ded4aa5e7cffe0ec666a9a608
NickNish09 <nnmarques97@gmail.com>
2018-11-12 22:44:06 -0200

    gerando body.html e puts com informacoes

--- templates_converter.rb
+++ templates_converter.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'nokogiri'
 
-puts "Convertendo template..."
+puts "Convertendo template...\n"
 
 doc = File.open("index.html")
 
@@ -14,25 +14,37 @@ markup.css('img').each do |el|
   el.replace "&lt;%= #{new_el} %&gt;"
 end
 
+puts "Cole isto entre as tags <head></head> do seu application.html.erb\n\n"
 markup.css('link').each do |el|
     href = el.get_attribute('href')
-    new_el = " stylesheet_link_tag \"#{href}\" ".to_s
+    new_el = "stylesheet_link_tag \"#{href}\"".to_s
     el.replace "&lt;%= #{new_el} %&gt;"
+    puts "<%= #{new_el} %>"
 end
+
+puts "\n"
+puts "Cole isto no final da tag <body> do seu application.html.erb, após o <%= yield %>\n\n"
 
 markup.css('script').each do |el|
     if(el.get_attribute('src').nil?)
     else
         source = el.get_attribute('src')
-        new_el = " javascript_include_tag \"#{source}\" ".to_s
+        new_el = "javascript_include_tag \"#{source}\"".to_s
         el.replace "&lt;%= #{new_el} %&gt;"
+        puts "<%= #{new_el} %>"
     end
+end
+
+puts "\nCole os conteúdos do arquivo body.html.erb na index.html.erb (sua página root) \n\n"
+
+markup.css('body').each do |el_body|
+    new_body = el_body.to_html.gsub(/&lt;/,"<").gsub(/&gt;/,">").gsub(/<%= javascript_include_tag (.+)/,"").gsub(/<body(.+)/,"").gsub("</body>","")
+    File.open("body.html.erb", "w") {|f| f.write(new_body) }
 end
 
 new_doc = markup.to_html.gsub(/&lt;/,"<").gsub(/&gt;/,">")
 
-
 File.open("index.html.erb", "w") {|f| f.write(new_doc) }
 
-
+puts "\n"
 puts "Template convertido"
