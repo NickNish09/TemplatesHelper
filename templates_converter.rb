d7ee931d6c5f3ca998da19a157927c9a78d704cc
NickNish09 <nnmarques97@gmail.com>
2018-11-12 22:03:23 -0200

    convertendo links, scripts e imagens

--- templates_converter.rb
+++ templates_converter.rb
@@ -1,21 +1,38 @@
 require 'rubygems'
 require 'nokogiri'
 
+puts "Convertendo template..."
+
 doc = File.open("index.html")
 
 markup = Nokogiri::HTML.parse(doc)
 
 markup.css('img').each do |el|
-  # puts el
   source = el.get_attribute('src')
   classe = el.get_attribute('class')
   new_el = " image_tag \"#{source}\", class: \"#{classe}\" ".to_s
-  puts new_el
   el.replace "&lt;%= #{new_el} %&gt;"
-  #puts el
+end
+
+markup.css('link').each do |el|
+    href = el.get_attribute('href')
+    new_el = " stylesheet_link_tag \"#{href}\" ".to_s
+    el.replace "&lt;%= #{new_el} %&gt;"
+end
+
+markup.css('script').each do |el|
+    if(el.get_attribute('src').nil?)
+    else
+        source = el.get_attribute('src')
+        new_el = " javascript_include_tag \"#{source}\" ".to_s
+        el.replace "&lt;%= #{new_el} %&gt;"
+    end
 end
 
 new_doc = markup.to_html.gsub(/&lt;/,"<").gsub(/&gt;/,">")
 
 
 File.open("index.html.erb", "w") {|f| f.write(new_doc) }
+
+
+puts "Template convertido"
